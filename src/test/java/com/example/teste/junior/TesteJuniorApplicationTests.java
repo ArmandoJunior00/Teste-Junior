fbeaeffec146ed2b1d067e614f4cea31f983d1c8
ArmandoJunior00 <armando.jr@sofintech.com.br>
2025-01-03 13:55:51 -0300

    commit inicial

---
+++ src/test/java/com/example/teste/junior/TesteJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste.junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
